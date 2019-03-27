d3bd449a9b51e7df5ce408fdbdcfd88c6a291074
Jason Cartwright <jason.cartwright44@gmail.com>
2019-03-26 23:51:28 -0700

    supervisor changes

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -17,39 +17,57 @@ connection.connect(function (error) {
     console.log('connected as id - after: ' + connection.threadId);
     //////////////////////////////////////////////////////////////////////////////
 
-function start() {
+    function start() {
 
-    inquirer
-    .prompt({
-        name: 'start',
-        type: 'list',
-        message: 'Please choose from the following list:',
-        choices: ['View Products Sales by Department', 'Create New Department', 'Quit']
-    }).then(function (answer) {
-        if (answer.start === 'Quit') {
-            connection.end();
-            }
-        if (answer.start === 'View Products Sales by Department') {
-            connection.query('SELECT * FROM departments ORDER BY department_name', function (error, res) {
-                if (error) {
-                    console.error(error);
+        inquirer
+            .prompt({
+                name: 'start',
+                type: 'list',
+                message: 'Please choose from the following list:',
+                choices: ['View Products Sales by Department', 'Create New Department', 'Quit']
+            }).then(function (answer) {
+                    if (answer.start === 'Quit') {
+                        connection.end();
+                    }
+                    if (answer.start === 'View Products Sales by Department') {
+                        connection.query('SELECT * FROM departments ORDER BY department_name', function (error, res) {
+                            if (error) {
+                                console.error(error);
+                            }
+                            console.table(res);
+                            // Here, create vars of total_profit, over_head_costs, and product_sales
+
+                            // Here, place them into variables
+
+                            
+                            // Here, join the two tables
+                        
+                        })
+                    }
+                    if (answer.start === 'Create New Department') {
+                        console.log('New')
+                        inquirer.prompt({
+                            name: 'name',
+                            type: 'input',
+                            message: 'What is the name of the new Department?'
+                        }).then(function (res) {
+                            var name = res.name;
+                            connection.query("INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES('" + name + "', '" + dept + "', '" + price + "', '" + quant + "')", function (error, response) {
+                                if (error) {
+                                    console.error(error)
+                                }
+                                connection.query('SELECT * FROM products', function (error, res) {
+                                    if (error) {
+                                        console.error(error);
+                                    }
+                                    console.table(res);
+                                    start();
+                                })
+                            })
+                        })
+                    }})
                 }
-                console.table(res);
             })
-        }
-        if (answer.start === 'Create New Department') {
-            
-        }
 
-
-
-
-
-
-
-
-
-}
 start();
-    //////////////////////////////////////////////////////////////////////////////
-})
+//////////////////////////////////////////////////////////////////////////////
